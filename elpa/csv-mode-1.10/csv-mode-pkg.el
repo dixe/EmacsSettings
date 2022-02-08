6d5099add5a0e57a9beb2277c2a780ca3fbcc90d
dixe <dixe@github.com>
2022-02-08 16:51:21 +0100

    Updated setting to new current settings

---
+++ elpa/csv-mode-1.10/csv-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "csv-mode" "1.10" "Major mode for editing comma/char separated values" (quote ((emacs "24.1") (cl-lib "0.5"))))
---
+++ elpa/elm-mode-0.21.0/elm-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "elm-mode" "0.21.0" "Major mode for Elm"
+  '((f "0.17")
+    (s "1.7.0")
+    (emacs "25.1")
+    (dash "2.13.0")
+    (reformatter "0.3"))
+  :commit "5df694e307cf8fa5a3555d800984aa4ebb40664f" :authors
+  (("Joseph Collard"))
+  :maintainer
+  ("Joseph Collard")
+  :url "https://github.com/jcollard/elm-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
